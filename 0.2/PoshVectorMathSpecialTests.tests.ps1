eb231aa70aae47b9279b26b8904798c703dabe65
Peter <pm@activetraining.de>
2018-01-30 22:07:47 +0100

    Added a test module

--- 0.2/PoshVectorMathSpecialTests.tests.ps1
+++ 0.2/PoshVectorMathSpecialTests.tests.ps1
@@ -9,12 +9,10 @@ using module .\VectorHelpers.psm1
 describe "more specialized tests with vectors" {
 
     it "calculates the distance between a point and a line" {
-
         $vA = v(1,1)
         $vB = v(3,3)
         $vP = v(2,3)
         $pD = Point2LineDistance($vA, $vB, $vP)
-        
     }
 
 }
